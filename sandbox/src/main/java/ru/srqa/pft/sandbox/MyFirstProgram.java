5adc5ec44c3f5069989a609c1d406168b77d5292
aibek17 <73880380+aibek17@users.noreply.github.com>
2021-11-17 22:39:18 +0600

    add functions

--- sandbox/src/main/java/ru/srqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/srqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,27 @@ package ru.srqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    hello("World");
+    hello("qwerty");
+    hello("asdf");
+
+    double k = 5;
+    System.out.println("area of object is " + area(k));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("square is " + area(a, b));
   }
 
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
